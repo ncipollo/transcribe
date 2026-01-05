5b6ea51a9ed00bf46979265651018f43e24ae1e2
Nick Cipollo <nicholas.cipollo@whoop.com>
2026-01-04 19:14:58 -0500

    Add action handler

---
+++ library/src/commonMain/kotlin/transcribe/action/ActionHandler.kt
@@ -0,0 +1,26 @@
+package transcribe.action
+
+import api.atlassian.AttachmentAPIClient
+
+class ActionHandler(
+    private val attachmentApiClient: AttachmentAPIClient,
+) {
+    /**
+     * Handles a list of transcriber actions by filtering for AttachmentDownload actions
+     * and downloading their data from the API.
+     *
+     * @param actions The list of transcriber actions to process
+     * @return A list of AttachmentResult objects containing the downloaded data and local paths
+     */
+    suspend fun handleActions(actions: List<TranscriberAction>): List<AttachmentResult> {
+        return actions
+            .filterIsInstance<AttachmentDownload>()
+            .map { action ->
+                AttachmentResult(
+                    data = attachmentApiClient.getAttachmentData(action.downloadPath),
+                    localRelativePath = action.localRelativePath,
+                )
+            }
+    }
+}
+
