c3a530ee17a86b462f61d5f8fa0821fd99476642
Nick Cipollo <nicholas.cipollo@whoop.com>
2026-01-09 11:57:30 -0500

    Tweaks to gradle and workflows
    - Maven should now auto-publish the release
    - Add versions task to list out updates.xx

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,18 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.benManesVersions) apply false
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    return !(stableKeyword || regex.matches(version))
 }
 
 subprojects {
     apply(plugin = "com.diffplug.spotless")
+    apply(plugin = "com.github.ben-manes.versions")
     
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         kotlin {
@@ -30,4 +38,18 @@ subprojects {
             endWithNewline()
         }
     }
+    
+    tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+        rejectVersionIf {
+            isNonStable(candidate.version)
+        }
+        
+        checkBuildEnvironmentConstraints = true
+        
+        outputFormatter = "plain"
+    }
+}
+
+tasks.register("dependencyUpdatesAll") {
+    dependsOn(subprojects.map { it.tasks.named("dependencyUpdates") })
 }
